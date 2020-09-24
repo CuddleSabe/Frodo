60a8e3d9f92514cf42e710aca639289dce2f6ecd
CuddleSabe <1565912498@qq.com>
2020-09-25 01:27:59 +0800

    The Start of FRODO

---
+++ Frodo.h
@@ -0,0 +1,58 @@
+#ifndef __FRODO_H__
+#define __FRODO_H__ 
+
+#include<stdio.h>
+#include<malloc.h>
+typedef struct matrix
+{
+	int row,cow;
+	double **matrix;
+}Matrix;
+
+Matrix* MatrixMultiply(Matrix *a, Matrix *b)	//矩阵乘法 
+{
+	int i,j,k;
+	Matrix *newMatrix;
+	newMatrix = (Matrix*)malloc(sizeof(Matrix));
+	
+	newMatrix->matrix = (double**)malloc(sizeof(double*)*(a->row));
+	for(i=0;i<a->row;i++)
+		newMatrix->matrix[i]= (double*)malloc(sizeof(double*)*(b->cow));
+	newMatrix->cow=b->cow;
+	newMatrix->row=a->row;
+	
+
+	for(i=0;i<a->row;i++)
+			for(j=0;j<b->cow;j++)
+				for(k=0;k<a->cow;k++) {
+					newMatrix->matrix[i][j]+=(a->matrix[i][k])*(b->matrix[k][j]);
+				}
+	return newMatrix;
+}
+
+void InitMatrix(Matrix *a)	//初始化矩阵 
+{
+	int i,j;
+	scanf("%d",&(a->row));
+	scanf("%d",&(a->cow));
+	a->matrix = (double**)malloc(sizeof(double*)*(a->row));
+	for(i=0;i<a->row;i++)
+		a->matrix[i]= (double*)malloc(sizeof(double*)*(a->cow));
+	for(i=0; i<a->row;i++)
+		for(j=0; j<a->cow;j++)
+			scanf("%lf",&(a->matrix[i][j]));
+}
+	
+void ShowMatrix(Matrix *a)	//矩阵展示 
+{	
+	int i,j;
+	for(i=0;i<a->row;i++)
+	{
+		for(j=0;j<a->cow;j++)
+			printf("%lf ",a->matrix[i][j]);
+		printf("\n");
+	}
+}
+
+#endif
+
